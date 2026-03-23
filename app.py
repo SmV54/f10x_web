f4bc27a14db1da064722390de8e68747f84f5371
Sergio <sergiomoraesvieira>
2026-03-23 10:49:26 -0300

    20260323e

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import re
+import os
 import random
 import smtplib
 import requests
@@ -262,5 +263,8 @@ def confirmar():
     return jsonify({"ok": True, "msg": "Cadastro realizado com sucesso"})
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    porta = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=porta, debug=True)
